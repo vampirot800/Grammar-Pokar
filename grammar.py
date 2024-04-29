a7ebd5dd63035dca261af68512d01908d3850cbf
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2024-04-29 15:16:45 -0600

    grammar definition 2.1

--- grammar.py
+++ grammar.py
@@ -12,25 +12,22 @@ nltk.download('punkt')
 # Context-free grammar defined
 grammar = CFG.fromstring
 ("""
-sentence -> play preposition combo hand
+sentence -> play 'of' combo hand
 
 play -> 'bets' | 'folds' | 'calls' | 'raises' | 'checks' | 'allIn'
 
-combo -> 'pair' 'of' card and card | 'twoPairs' 'of' rank 'and' rank | 'threeOfKind'
+combo -> 'pair' 'of' card 'and' card | 'twoPairs' 'of' rank 'and' rank | 'threeOfKind'
  'of' rank | 'straight' 'of' rank rank rank rank card | 'flush' 'of' rank rank rank rank rank
 | 'fullHouse' 'of' rank rank 'and' rank rank rank | 'pokar' 'of' rank | 
 'straightFlush' 'of' rank rank rank rank card | 'RoyalFlush'
 
 hand -> card card
 
-card -> rank preposition suit
+card -> rank 'of' suit
 
 rank -> '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'J' | 'Q' | 'K' | 'A'
 
 suit -> 'hearts' | 'diamonds' | 'clubs' | 'spades'
-
-preposition -> 'of' | 'on'
-
 """)
 
 # Parser with defined grammar
