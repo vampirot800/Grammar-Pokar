037b530810d8096ed3f748c73a1c56687f522f2b
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2024-06-05 02:22:50 -0600

    Effectiveness + test cases

--- grammar.py
+++ grammar.py
@@ -10,21 +10,22 @@ nltk.download('punkt')
 
 # Context-free grammar defined
 grammar = CFG.fromstring("""
-test -> play combo
-play -> 'bets' 'on' | 'folds' 'with' | 'raises' 'with'| 'checks' 'with' | 'goes' 'all' 'in'
+test -> play preposition combo
+play -> 'bets' | 'folds' | 'raises' | 'calls' |'checks'| 'goes' 'all' 'in'
 combo -> royalFlush | straightFlush | fullHouse | pokar | straight | flush | threeOfKind | twoPairs | pair | highCard
-royalFlush -> 'royal' 'flush' 'AKQJ10' 'of' suit
-straightFlush -> 'straight' 'flush' 'of' rank rank rank rank card
-fullHouse -> 'full' 'house' 'of' pair threeOfKind
-pokar -> 'four' 'of' 'a' 'kind' 'of' rank
-straight -> 'straight' 'of' rank rank rank rank rank
-flush -> 'flush' 'of' suit
-threeOfKind -> 'three' 'of' rank
-twoPairs -> 'two' 'pairs' 'of' rank 'and' rank
-pair -> 'pair' 'of' rank
-highCard -> 'high' 'card' 'of' rank
+royalFlush -> 'royal' 'flush' 'AKQJ10' preposition suit
+straightFlush -> 'straight' 'flush' preposition rank rank rank rank card
+fullHouse -> 'full' 'house' preposition pair preposition threeOfKind
+pokar -> 'four' preposition 'a' 'kind' preposition rank
+straight -> 'straight' preposition rank rank rank rank rank
+flush -> 'flush' preposition suit
+threeOfKind -> 'three' preposition rank
+twoPairs -> 'two' 'pairs' preposition rank 'and' rank
+pair -> 'pair' preposition rank
+highCard -> 'high' 'card' preposition card
 hand -> card card
-card -> rank 'of' suit
+card -> rank preposition suit
+preposition -> 'of' | 'with' | 'on' 
 rank -> '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'J' | 'Q' | 'K' | 'A'
 suit -> 'hearts' | 'diamonds' | 'clubs' | 'spades'
 """)
@@ -34,9 +35,13 @@ parser = nltk.ChartParser(grammar)
 
 # List of test sentences to be parsed
 sentences = [
-    "bets on full house of pair of 5 three of 6",
+    "bets on full house of pair of 5 with three of 6",
     "raises with royal flush AKQJ10 of spades",
-    "folds with high card of K"
+    "folds with high card of K of clubs",
+    "calls on straight of 3 4 5 6 7",
+    "checks with four of a kind of K",
+    "goes all in with straight flush of 7 8 9 J Q of diamonds"
+
 ]
 
 # Function to parse and display the parse trees
@@ -50,4 +55,6 @@ def parse_sentences(sentences):
 
 # Parse the list of sentences
 parse_sentences(sentences)
+
+
 
